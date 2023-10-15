b5087587fa8124205b83393ba7aacaffb589d570
Md Munna Mia <mdmunnamia987@gmail.com>
2023-10-15 22:07:11 +0600

    adding draw case

--- src/script.js
+++ src/script.js
@@ -1,7 +1,8 @@
 let gameStart = true;
 let playerMove = "X";
-const soundClick = new Audio('../src/Sounds/click-button-140881.mp3')
-const successSound = new Audio('../src/Sounds/game-bonus-144751.mp3')
+const soundClick = new Audio("../src/Sounds/click-button-140881.mp3");
+const successSound = new Audio("../src/Sounds/game-bonus-144751.mp3");
+const drawSound = new Audio("../src/Sounds/wah-wah-sad-trombone-6347.mp3");
 //wining moves
 const winingMoves = [
   [0, 1, 2],
@@ -26,11 +27,11 @@ gameTitle.innerText = "TIC TAC TOE";
 gameTitle.classList.add("gameTitle");
 root.appendChild(gameTitle);
 
-// winner anoucment 
-const winnerAnnoucment = document.createElement('p')
-winnerAnnoucment.classList.add('winnerName')
-winnerAnnoucment.innerText = 'Welcome'
-root.appendChild(winnerAnnoucment)
+// winner anoucment
+const winnerAnnoucment = document.createElement("p");
+winnerAnnoucment.classList.add("winnerName");
+winnerAnnoucment.innerText = "Welcome";
+root.appendChild(winnerAnnoucment);
 
 //game container
 const gameContainer = document.createElement("div");
@@ -38,12 +39,10 @@ gameContainer.classList.add("gameContainerStyle");
 root.appendChild(gameContainer);
 
 //game reset button
-const gameReset = document.createElement('div');
-gameReset.innerHTML = '<i class="fa-solid fa-repeat"></i>'
-gameReset.classList.add('resetButton')
-root.appendChild(gameReset)
-
-
+const gameReset = document.createElement("div");
+gameReset.innerHTML = '<i class="fa-solid fa-repeat"></i>';
+gameReset.classList.add("resetButton");
+root.appendChild(gameReset);
 
 //check box
 for (let row = 0; row < 3; row++) {
@@ -57,35 +56,51 @@ for (let row = 0; row < 3; row++) {
   }
 }
 
-gameReset.addEventListener("click", ()=>{
+gameReset.addEventListener("click", () => {
   const cells = document.querySelectorAll(".cellStyle");
-  cells.forEach(item =>{
-    item.innerText = ''
-  })
-  gameStart = true  
-  winnerAnnoucment.innerText = 'Welcome'
-})
+  cells.forEach((item) => {
+    item.innerText = "";
+  });
+  gameStart = true;
+  winnerAnnoucment.innerText = "Welcome";
+});
 
-function clickEventHandler(e){
-  soundClick.volume = 0.5
+function clickEventHandler(e) {
+  soundClick.volume = 0.5;
   let cell = e.target;
-  if (!gameStart || cell.innerText !== "") return;//direct return
+  if (!gameStart || cell.innerText !== "") return; //direct return
   cell.innerText = playerMove;
   playerMove = playerMove === "X" ? "O" : "X";
   soundClick.play();
 
   //wining or lose chech
   const cells = document.querySelectorAll(".cellStyle");
-  winingMoves.forEach(item => {
+
+  winingMoves.forEach((item) => {
     let [a, b, c] = item;
     if (
       cells[a].innerText !== "" &&
       cells[a].innerText === cells[b].innerText &&
       cells[b].innerText === cells[c].innerText
     ) {
-      winnerAnnoucment.innerText = `Player ${cells[a].innerText} Win This Match `
-      successSound.play()
+      winnerAnnoucment.innerText = `Player ${cells[a].innerText} Win This Match `;
+      successSound.play();
       gameStart = false;
+    } else {
+      drawCheaker(cells);
     }
-  })
+  });
+}
+
+function drawCheaker(cell) {
+  newCheckArray = [];
+  for (let i of cell) {
+    if (i.innerText !== "") {
+      newCheckArray.push(i);
+      if (newCheckArray.length === 9) {
+        winnerAnnoucment.innerText = `Player Draw This Match `;
+        drawSound.play();
+      }
+    }
+  }
 }
